05dd46092253017e2246aa9ea5185df17e9a4364
Dax <Daxtubach@gmail.com>
2019-10-22 01:37:56 -0400

    Updates

---
+++ precache-manifest.c9f69aed1d9726d58f7a7c8fa0d552cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39d355c07270003dfeec3ac2a29d8bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08a1c3c1dda393aa8451",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4290502b3ff3727da7c8",
+    "url": "/static/js/2.bda75e5a.chunk.js"
+  },
+  {
+    "revision": "08a1c3c1dda393aa8451",
+    "url": "/static/js/main.27551f28.chunk.js"
+  },
+  {
+    "revision": "6bf6c78eae2d44d7080d",
+    "url": "/static/js/runtime-main.5545d96f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
